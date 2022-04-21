76d58abc542c09894ec95b0846c54a6da3e09f42
Ambili das v s <ambili.kan@gmail.com>
2022-04-21 15:22:56 +0530

    check emp is present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+EMP_WAGE_PER_HOUR=20;
+PRESENT=1;
+WORKING_HOUR=8;
+
+ispresent=$((RANDOM%2));
+
+if [ $ispresent -eq $PRESENT ]
+then
+  dailywage=$((WORKING_HOUR * EMP_WAGE_PER_HOUR));
+  echo "employee is present ";
+else
+  dailywage=0;
+  echo "employee is absent ";
+fi
+
+echo "employee daily wage: "$dailywage "USD";
+
 

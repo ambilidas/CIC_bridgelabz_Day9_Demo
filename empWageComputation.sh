39d9e6a965dbaa75f90bee6e914d9cdb52374e14
Ambili das v s <ambili.kan@gmail.com>
2022-04-21 17:06:47 +0530

    calculate monthly wages of employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,27 +4,42 @@ EMP_WAGE_PER_HOUR=20;
 PRESENT=1;
 WORKING_HOUR=8;
 PART_TIME=2;
+MAX_WORKING_DAY=20;
+
+totalsalary=0;
+
+for ((day-0;day<$MAX_WORKING_DAY;day++))
+do
 
 ispresent=$((RANDOM%3));
+
 
 case $ispresent in
 
    $PRESENT)
 
      dailywage=$((WORKING_HOUR * EMP_WAGE_PER_HOUR));
-     echo "employee is present ";
+
    ;;
 
    $PART_TIME)
 
       dailywage=$((WORKING_HOUR/2 * EMP_WAGE_PER_HOUR));
-      echo "employee is working part time";
+
    ;;
 
    *)
 
       dailywage=0;
-      echo "employee is absent ";
+
    ;;
 esac;
+
+echo $dailywage;
+totalsalary=$((totalsalary + dailywage ));
+done;
+
+echo "employee monthly wage: $"$totalsalary "USD";
+
+
 

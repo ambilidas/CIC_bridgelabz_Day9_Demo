5a8a0fed0638df08f761169ef1773f796f3cb5b4
Ambili das v s <ambili.kan@gmail.com>
2022-04-21 17:16:03 +0530

    calculate wage with a condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,44 +2,55 @@
 
 EMP_WAGE_PER_HOUR=20;
 PRESENT=1;
-WORKING_HOUR=8;
 PART_TIME=2;
+WORKING_HOUR=8;
 MAX_WORKING_DAY=20;
+MAX_WORKING_HOUR=40;
 
-totalsalary=0;
+totalworkinghour=0;
+day=0;
 
-for ((day-0;day<$MAX_WORKING_DAY;day++))
+while [[ $day -lt $MAX_WORKING_DAY && $totalworkinghour -lt $MAX_WORKING_HOUR ]]
 do
 
-ispresent=$((RANDOM%3));
+   if [ $totalworkinghour -eq $((MAX_WORKING_HOUR - WORKING_HOUR/2)) ]
+   then
+      ispresent=$PART_TIME;
+   else
 
+      ispresent=$((RANDOM%3));
+   fi
 
-case $ispresent in
+   case $ispresent in
 
-   $PRESENT)
+      $PRESENT)
 
-     dailywage=$((WORKING_HOUR * EMP_WAGE_PER_HOUR));
+         empHr=$WORKING_HOUR;
 
-   ;;
+      ;;
 
-   $PART_TIME)
+      $PART_TIME)
 
-      dailywage=$((WORKING_HOUR/2 * EMP_WAGE_PER_HOUR));
+         empHr=$((WORKING_HOUR/2));
 
-   ;;
+      ;;
 
-   *)
+      *)
 
-      dailywage=0;
+         empHr=0;
 
-   ;;
-esac;
-
-echo $dailywage;
-totalsalary=$((totalsalary + dailywage ));
+      ;;
+   esac;
+   totalworkinghour=$((totalworkinghour + empHr));
+   ((day++));
 done;
 
+totalsalary=$((totalworkinghour * EMP_WAGE_PER_HOUR));
+
+echo "employee total working hour:" $totalworkinghour;
 echo "employee monthly wage: $"$totalsalary "USD";
+echo "employee total working days:" $day;
+
 
 
 

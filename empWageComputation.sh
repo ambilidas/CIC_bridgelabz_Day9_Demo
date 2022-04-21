2357dfd2b5bfb1da3847f586cddf7d697b641844
Ambili das v s <ambili.kan@gmail.com>
2022-04-21 16:27:44 +0530

    part time is added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,6 +3,7 @@
 EMP_WAGE_PER_HOUR=20;
 PRESENT=1;
 WORKING_HOUR=8;
+PART_TIME=2;
 
 ispresent=$((RANDOM%2));
 
@@ -11,6 +12,11 @@ then
   
   echo "employee is present ";
   dailywage=$((WORKING_HOUR * EMP_WAGE_PER_HOUR));
+elif [ $ispresent -eq $PART_TIME ]
+then
+   dailywage=$((WORKING_HOUR/2 * EMP_WAGE_PER_HOUR));
+   echo "employee is working part time";
+
 else
   dailywage=0;
   echo "employee is absent ";
